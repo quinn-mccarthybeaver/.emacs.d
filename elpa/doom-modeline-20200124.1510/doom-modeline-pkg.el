1bea6ba6e28aceb5e59b84219582ecc16efecb1f
quinn-mccarthybeaver <quinn.mccarthybeaver@gmail.com>
2020-01-27 12:54:36 -0500

    gitignore added

--- elpa/doom-modeline-20200124.1510/doom-modeline-pkg.el
+++ elpa/doom-modeline-20200124.1510/doom-modeline-pkg.el
@@ -1,4 +1,4 @@
-(define-package "doom-modeline" "20200118.1711" "A minimal and modern mode-line"
+(define-package "doom-modeline" "20200124.1510" "A minimal and modern mode-line"
   '((emacs "25.1")
     (all-the-icons "1.0.0")
     (shrink-path "0.2.0")
